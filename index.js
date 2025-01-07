f3783639695390c2048131976c6e9bec68e5f884
+ <joshngaara@gmail.com>
2025-01-07 16:59:36 +0200

    grade rmv comm

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ function studentGrade(studentMarks) {
 
     if (studentMarks>79) {
         grade= "A";
-        // console.log(`Grade is ${grade} `);
     }else if(studentMarks>=60&&studentMarks<=79){
         grade= "B";
-        // console.log(`Grade is ${grade} `);
     }else if(studentMarks>=49&&studentMarks<=50){
         grade= "C";
-        // console.log(`Grade is ${grade} `);
     }
     else if(studentMarks>=40&&studentMarks<=49){
         grade= "D";
-        // console.log(`Grade is ${grade} `);
     }else if(studentMarks<40){
         grade= "E";
-        // console.log(`Grade is ${grade} `);
     }
-    
+
     console.log(`Grade student attained is ${grade} .`);
 }
 
@@ -87,8 +82,6 @@ function speedDetector(carSpeed){
 
     return message;
 }
-let x =studentGrade(64)
-// console.log(x);
 studentGrade(70);
 studentGrade(80);
 studentGrade(60);
