34c8144144e17b754f204f3c6c0f9af0693d98b0
+ <joshngaara@gmail.com>
2025-01-07 16:54:42 +0200

    spd detector

--- index.js
+++ index.js
@@ -1,27 +1,111 @@
 function studentGrade(studentMarks) {
     let grade = "";
-    console.log(`m is ${studentMarks} `);
+    var output = '';
 
     if (studentMarks>79) {
         grade= "A";
-        console.log(`Grade is ${grade} `);
+        // console.log(`Grade is ${grade} `);
     }else if(studentMarks>=60&&studentMarks<=79){
         grade= "B";
-        console.log(`Grade is ${grade} `);
+        // console.log(`Grade is ${grade} `);
     }else if(studentMarks>=49&&studentMarks<=50){
         grade= "C";
-        console.log(`Grade is ${grade} `);
+        // console.log(`Grade is ${grade} `);
     }
     else if(studentMarks>=40&&studentMarks<=49){
         grade= "D";
-        console.log(`Grade is ${grade} `);
+        // console.log(`Grade is ${grade} `);
     }else if(studentMarks<40){
         grade= "E";
-        console.log(`Grade is ${grade} `);
+        // console.log(`Grade is ${grade} `);
     }
     
     console.log(`Grade student attained is ${grade} .`);
 }
 
+var licensePoints= 0;
+var speedLimit = 70;
+var message ='';
+
+function speedDetector(carSpeed){
+    licensePoints=0;
+    if (carSpeed<=70) {
+        message = `Ok`;    
+        console.log(`${message} `);
+    }else if(carSpeed>70&&carSpeed<=75){
+        licensePoints+= 1;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed>=speedLimit+5 &&carSpeed<=speedLimit+10){
+        licensePoints+= 2;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+15 &&carSpeed>=speedLimit+10){
+        licensePoints+= 3;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+20 &&carSpeed>=speedLimit+15){
+        licensePoints+= 4;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+25 &&carSpeed>=speedLimit+20){
+        licensePoints+= 5;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+30 &&carSpeed>=speedLimit+25){
+        licensePoints+= 6;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+35 &&carSpeed>=speedLimit+30){
+        licensePoints+= 7;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+40 &&carSpeed>speedLimit+35){
+        licensePoints+= 8;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+45 &&carSpeed>=speedLimit+40){
+        licensePoints+= 9;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+50 &&carSpeed>=speedLimit+45){
+        licensePoints+= 10;
+        message = `Points: ${licensePoints} `
+
+    }else if(carSpeed<=speedLimit+55 &&carSpeed>=speedLimit+50){
+        licensePoints+= 11;
+        message = `Points: ${licensePoints} `
+
+    }
+    else if(carSpeed>=speedLimit+60 ){
+        licensePoints+= 12;
+        message = `License Suspended `
+
+    }
+
+    console.log((message));
+
+    return message;
+}
 let x =studentGrade(64)
-console.log(x);
+// console.log(x);
+studentGrade(70);
+studentGrade(80);
+studentGrade(60);
+studentGrade(50);
+studentGrade(40);
+studentGrade(39);
+
+speedDetector(70);
+speedDetector(75);
+speedDetector(80);
+speedDetector(85);
+speedDetector(90);
+speedDetector(95);
+speedDetector(100);
+speedDetector(105);
+speedDetector(110);
+speedDetector(115);
+speedDetector(120);
+speedDetector(125);
+speedDetector(130);
